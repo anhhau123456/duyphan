7a9ce0e2b8889706c4d5a4b17771e87a296b1559
Hau Nguyen <hau.nguyentrinh@asnet.com.vn>
2025-05-11 18:12:44 +0700

    Aggrid

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css'; // or another themes
 import { Auth0Provider } from '@auth0/auth0-react';
 
 import './index.css'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,40 @@
 import { AgGridReact } from 'ag-grid-react';
 import { useState } from 'react';
-import { ClientSideRowModelModule, themeQuartz } from 'ag-grid-community';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { ClientSideRowModelModule, themeAlpine } from 'ag-grid-community';
 
 export default function MyGrid() {
-    const [rowData] = useState([
-      { make: 'Toyota', model: 'Celica', price: 35000 },
-      { make: 'Ford', model: 'Mondeo', price: 32000 },
-      { make: 'Porsche', model: 'Boxster', price: 72000 },
-    ]);
+    const rowData = [
+        { name: 'John', age: 24, country: 'USA' },
+        { name: 'Alice', age: 30, country: 'Canada' },
+        { name: 'Bob', age: 27, country: 'UK' },
+        { name: 'Lisa', age: 35, country: 'Germany' },
+    ];
 
-    const [columnDefs] = useState([
-        {
-            headerName: '',
-            checkboxSelection: true,
-            headerCheckboxSelection: true,
-            width: 50,
-        },
-      { field: 'make' },
-      { field: 'model' },
-      { field: 'price' },
-    ]);
+    const columnDefs = [
+        { headerName: 'Name', field: 'name', checkboxSelection: true, headerCheckboxSelection: true },
+        { headerName: 'Age', field: 'age' },
+        { headerName: 'Country', field: 'country' },
+      ];
 
-    const theme = themeQuartz
+
+    const theme = themeAlpine
         .withParams({
             browserColorScheme: "light",
             headerFontSize: 14
         });
 
     return (
-        <div>
-            <div className="ag-theme-alpine w-screen" style={{ height: 400, width: '100%' }}>
-                <AgGridReact
-                    rowData={rowData}
-                    columnDefs={columnDefs}
-                    modules={[ClientSideRowModelModule]}
-                    theme={theme}
-                    animateRows={true}
-                    pagination={true}
-                    paginationPageSize={2}
-                    rowSelection="multiple"
-                />
-            </div>
+        <div className="ag-theme-alpine" style={{ height: 400, width: '100%' }}>
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={columnDefs}
+                modules={[ClientSideRowModelModule]}
+                theme={theme}
+                animateRows={true}
+                pagination={true}
+                paginationPageSize={2}
+                rowSelection="multiple"
+            />
         </div>
     );
   }
