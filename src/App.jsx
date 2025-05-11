3fea7c96eab22cf475c3f689e68b31d16b45aa82
Hau Nguyen <hau.nguyentrinh@asnet.com.vn>
2025-05-11 17:29:00 +0700

    add auth0

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
+  Button,
   Typography,
   IconButton,
   Drawer,
@@ -10,76 +11,97 @@ import {
   ListItem,
   ListItemText,
 } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useAuth0 } from '@auth0/auth0-react';
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 import Home from './pages/Home';
 
-import Sidebar from './components/Sidebar';
-
 function App() {
+  const {
+    loginWithRedirect,
+    logout,
+    isAuthenticated,
+    isLoading,
+    user,
+    error
+  } = useAuth0();
   const [open, setOpen] = useState(false);
   const drawerWidth = 240;
 
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className="flex h-screen">
-      {/* Main Content Area */}
-      <div className="flex-1 flex flex-col">
-        {/* Top AppBar */}
-        <AppBar position="static" className="z-10">
-          <Toolbar className="justify-between">
-            <IconButton
-              color="inherit"
-              edge="start"
-              onClick={() => setOpen(!open)}
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              My App
-            </Typography>
-          </Toolbar>
-        </AppBar>
-
-        {/* Page Content */}
-        <div className="flex flex-1">
-          {/* Drawer inside the page */}
-          <Drawer
-            variant="temporary"
-            anchor="left"
-            open={open}
-            onClose={() => setOpen(false)}
-            sx={{
-              width: drawerWidth,
-              flexShrink: 0,
-              '& .MuiDrawer-paper': {
+      {isAuthenticated ? (
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      ) : (
+        <div className="flex-1 flex flex-col">
+          <AppBar position="static" className="z-10">
+            <Toolbar className="justify-between">
+              <IconButton
+                color="inherit"
+                edge="start"
+                onClick={() => setOpen(!open)}
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography>
+                Phone SMS
+              </Typography>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: 'coral', // or any hex/code
+                  '&:hover': {
+                    backgroundColor: '#1565c0', // darker shade on hover
+                  },
+                }}
+                className="ml-auto px-4 py-2 bg-blue-600 text-white rounded"
+                onClick={() => logout()}>
+                Log Out
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <div className="flex flex-1">
+            {/* Drawer inside the page */}
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={open}
+              onClose={() => setOpen(false)}
+              sx={{
                 width: drawerWidth,
-                boxSizing: 'border-box',
-              },
-            }}
-          >
-            <List>
-              {['Home'].map((text) => (
-                <ListItem button key={text}>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
-          </Drawer>
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                  width: drawerWidth,
+                  boxSizing: 'border-box',
+                },
+              }}
+            >
+              <List>
+                {['Home', 'History', 'Import'].map((text) => (
+                  <ListItem button key={text}>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
 
-          {/* Main Content Area */}
-          <main className="flex-1 bg-gray-100 p-6">
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </main>
+            {/* Main Content Area */}
+            <main className="flex-1 bg-gray-100 p-6">
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </main>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css'; // or another themes
+import { Auth0Provider } from '@auth0/auth0-react';
+
+import './index.css'
+import App from './App.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StrictMode>
+  </Auth0Provider>
 )
