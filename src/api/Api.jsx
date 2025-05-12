652ca3407ba70f1d45fbd4154180ef444db9f0fb
Hau Nguyen <ntahau1989@gmail.com>
2025-05-12 16:17:57 +0700

    send sms

---
+++ src/api/Api.jsx
@@ -0,0 +1,66 @@
+const host = import.meta.env.VITE_HOST
+
+const fetchData = async () => {
+    try {
+        const res = await fetch(`${host}/fetch-data`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const json = await res.json();
+        
+        return json
+    } catch (err) {
+        console.error('Upload failed:', err);
+    }
+};
+
+const uploadFile = async (rows) => {
+    try {
+        const res = await fetch(`${host}/upload-file`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ data: rows }),   // rows = array of objects
+        });
+        const json = await res.json();
+
+        return json
+    } catch (err) {
+        console.error('Upload failed:', err);
+    }
+};
+
+const fetchHistories = async () => {
+    try {
+        const res = await fetch(`${host}/fetch-histories`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const json = await res.json();
+        
+        return json
+    } catch (err) {
+        console.error('Upload failed:', err);
+    }
+};
+
+const sendSMS = async (people) => {
+    try {
+        const res = await fetch(`${host}/send-sms`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ data: people }),   // rows = array of objects
+        });
+        const json = await res.json();
+
+        return json
+    } catch (err) {
+        console.error('Upload failed:', err);
+    }
+};
+
+export default {
+    fetchData,
+    uploadFile,
+    fetchHistories,
+    sendSMS
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,61 +1,60 @@
 import { AgGridReact } from 'ag-grid-react';
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import * as XLSX from 'xlsx';
 import { themeMaterial } from 'ag-grid-community';
+import {
+    AppBar,
+    Toolbar,
+    Button,
+    TextField,
+    Typography,
+    IconButton,
+    Drawer,
+    List,
+    ListItem,
+    ListItemText,
+  } from '@mui/material';
+
+
+import Api from '../api/Api';
 
 import InputPopup from '../components/InputPopup'
 
 export default function MyGrid() {
+    const gridRef = useRef();
+
     const [rowData, setRowData] = useState([]);
+
+    const [message, setMessage] = useState('');
 
     const colDefs = [
         {
             headerCheckboxSelection: true,
             checkboxSelection: true,
             pinned: 'left',
-            field: 'ID',
-            headerName: 'ID',
+            field: 'Name',
+            headerName: 'Name',
         },
-        { field: "Name" },
-        { field: "Phone Number" },
+        { field: "PhoneNumber" },
         { field: "Location" }
     ]
 
-    const handleFileUpload = (e) => {
-        const file = e.target.files[0];
-        if (!file) return;
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await Api.fetchData();
+            setRowData(data.items);
+        }
+        fetchData()
+    }, []);
 
-        const reader = new FileReader();
+    
+    const handleGetSelected = () => {
+        const selectedRows = gridRef.current.api.getSelectedRows();
 
-        reader.onload = (event) => {
-            const binaryStr = event.target.result;
-            const workbook = XLSX.read(binaryStr, { type: 'binary' });
-
-            // Get first sheet
-            const sheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[sheetName];
-
-            // Convert to JSON
-            const rows  = XLSX.utils.sheet_to_json(worksheet);
-            setRowData(rows);
-            sendToBackend(rows);  
-        };
-
-        reader.readAsBinaryString(file);
-        
-    };
-
-    const sendToBackend = async (rows) => {
-        try {
-            const res = await fetch('http://localhost:5000/upload-xlsx', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ data: rows }),   // rows = array of objects
-            });
-            const json = await res.json();
-            console.log('Server reply:', json);
-        } catch (err) {
-            console.error('Upload failed:', err);
+        if (message == '' || selectedRows.length == 0) {
+            console.log('error')
+        } else {
+            console.log('ok')
         }
     };
 
@@ -72,17 +71,41 @@ export default function MyGrid() {
 
     return (
         <div className="ag-theme-alpine h-[calc(100vh-10rem)] " style={{ width: '100%' }}>
-            <div className="ag-grid-custom">
-                <AgGridReact
-                    theme={aggridTheme}
-                    rowData={rowData}
-                    columnDefs={colDefs}
-                    pagination={true}
-                    paginationPageSize={20}
-                    rowSelection={"multiple"}
-                />
+            <div className="flex items-center mb-4">
+                <div className="flex items-center mb-4 mr-4">
+                    <TextField
+                        multiline
+                        minRows={1}
+                        maxRows={10}
+                        label="Message"
+                        variant="outlined"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        fullWidth
+                    />
+                </div>
+                <Button
+                    variant="contained"
+                    sx={{
+                        backgroundColor: '#746bca',
+                        '&:hover': {
+                            backgroundColor: '#272168',
+                        },
+                    }}
+                    className="ml-auto px-4 py-2 bg-blue-600 text-white rounded"
+                    onClick={() => handleGetSelected()}>
+                    Send
+                </Button>
             </div>
-            
+            <AgGridReact
+                ref={gridRef}
+                theme={aggridTheme}
+                rowData={rowData}
+                columnDefs={colDefs}
+                pagination={true}
+                paginationPageSize={20}
+                rowSelection={"multiple"}
+            />
         </div>
     );
   }
